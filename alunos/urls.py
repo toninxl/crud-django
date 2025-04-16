11c56f2c288764e8697faed7df0191011691751f
Antonio <antonio346753@gmail.com>
2025-04-15 21:16:41 -0300

    Adiciona funcionalidades de deletar e atualizar alunos, além de exibir lista de alunos na página de criação. Atualiza o template HTML para suportar as novas funcionalidades.

--- alunos/urls.py
+++ alunos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('criar_aluno/', views.criar_aluno, name='criar_aluno'),
+    path('deletar_aluno/<int:id>', views.deletar_aluno, name='deletar_aluno'),
+    path('atualizar_aluno/<int:id>', views.atualizar_aluno, name='atualizar_aluno'),
 ]
 
--- alunos/views.py
+++ alunos/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Aluno
 
 # Create your views here.
 def criar_aluno(request):
     if request.method == "GET":
-        return render(request, 'alunos/criar_alunos.html')
+        alunos = Aluno.objects.all()
+        
+        return render(request, 'alunos/criar_alunos.html', {"alunos": alunos})
     elif request.method == "POST":
         nome = request.POST.get("nome")
         email = request.POST.get("email")
@@ -15,3 +17,19 @@ def criar_aluno(request):
         
         return redirect("criar_aluno")
         
+def deletar_aluno(request, id):
+    aluno = get_object_or_404(Aluno, id=id)
+    aluno.delete()
+    return redirect("criar_aluno")
+
+def atualizar_aluno(request,id):
+    aluno = get_object_or_404(Aluno, id=id)
+    nome = request.POST.get("nome")
+    email = request.POST.get("email")
+    
+    aluno.nome = nome
+    aluno.email = email
+    
+    aluno.save()
+    
+    return redirect("criar_aluno")
